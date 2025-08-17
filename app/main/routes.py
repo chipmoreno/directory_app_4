63d357a14d3881276fe29b50523442c8c6a9a32c
chipmoreno <chip.moreno@gmail.com>
2025-08-17 13:02:38 -0500

    .

--- app/main/routes.py
+++ app/main/routes.py
@@ -17,3 +17,31 @@ def index():
 def profile():
     # current_user is automatically available from Flask-Login
     return render_template('profile.html', title='Your Profile', user=current_user)
+
+@main_bp.route('/todays_posts')
+def todays_posts():
+    return render_template('todays_posts.html', title='Today\'s Posts')
+
+@main_bp.route('/real_estate')
+def real_estate():
+    return render_template('real_estate.html', title='Real Estate')
+
+@main_bp.route('/user_forum')
+def user_forum():
+    return render_template('user_forum.html', title='User Forum')
+
+@main_bp.route('/marketplace')
+def marketplace():
+    return render_template('marketplace.html', title='Marketplace')
+
+@main_bp.route('/business_announcements')
+def business_announcements():
+    return render_template('business_announcements.html', title='Business Announcements')
+
+@main_bp.route('/events_accommodations')
+def events_accommodations():
+    return render_template('events_accommodations.html', title='Events & Accommodations')
+
+@main_bp.route('/best_of')
+def best_of():
+    return render_template('best_of.html', title='Best Of')
