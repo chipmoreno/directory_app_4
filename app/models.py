956e04b213131c804ff95a2d74f9a64a718e78fa
chipmoreno <chip.moreno@gmail.com>
2025-08-17 12:32:25 -0500

    .

--- app/models.py
+++ app/models.py
@@ -29,7 +29,8 @@ class User(db.Model, UserMixin, SerializerMixin):
     username = db.Column(db.String(64), unique=True, nullable=False)
     email = db.Column(db.String(120), unique=True, nullable=False)
     password_hash = db.Column(db.String(128))
-    created_on = db.Column(db.DateTime, default=datetime.utcnow)
+    is_active = db.Column(db.Boolean, default=True)
+    created_at = db.Column(db.DateTime, default=datetime.utcnow)
     posts = db.relationship('Post', backref='author', lazy=True)
     '''roles = db.relationship('Role', secondary=user_roles, backref=db.backref('users', lazy='dynamic'))'''
     
